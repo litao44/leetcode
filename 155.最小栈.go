b874ef8d3ac95799a90aaa4a5b3e5c20873d8d9f
litao <litao9206@gmail.com>
2021-01-04 22:49:48 +0800

    min stack

--- 155.最小栈.go
+++ 155.最小栈.go
@@ -6,32 +6,36 @@
 
 // @lc code=start
 type MinStack struct {
-	stack stack
-	min   int
+	data *stack
+	min  *stack
 }
+
+const INT_MAX = int(^uint(0) >> 1)
 
 /** initialize your data structure here. */
 func Constructor() MinStack {
-
-}
-
-func (ms *MinStack) Push(x int) {
-	ms.stack.Push(x)
-	if x < ms.min {
-		ms.min = x
+	return MinStack{
+		data: &stack{make([]int, 0, 1)},
+		min:  &stack{[]int{INT_MAX}},
 	}
 }
 
-func (ms *MinStack) Pop() {
+func (ms *MinStack) Push(x int) {
+	ms.data.Push(x)
+	ms.min.Push(min(x, ms.min.Top()))
+}
 
+func (ms *MinStack) Pop() {
+	ms.data.Pop()
+	ms.min.Pop()
 }
 
 func (ms *MinStack) Top() int {
-
+	return ms.data.Top()
 }
 
 func (ms *MinStack) GetMin() int {
-
+	return ms.min.Top()
 }
 
 type stack struct {
@@ -42,25 +46,22 @@ func (s *stack) Push(e int) {
 	s.data = append(s.data, e)
 }
 
-func (s *stack) IsEmpty() bool {
-	return len(s.data) == 0
-}
-
 func (s *stack) Top() int {
-	if s.IsEmpty() {
-		return 0
-	}
-	return s.data[len(s.s)-1]
+	return s.data[len(s.data)-1]
 }
 
 func (s *stack) Pop() (int, bool) {
-	if s.IsEmpty() {
-		return 0, false
-	}
 	l := len(s.data)
 	ret := s.data[l-1]
-	s.s = s.data[:l-1]
+	s.data = s.data[:l-1]
 	return ret, true
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
 
 /**
